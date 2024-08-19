82b311602e5bce57416273fe5248b83bd3315289
Sweapz <nillerduden@hotmail.com>
2024-08-19 03:25:49 +0200

    Added initial inventory component

---
+++ src/app/sheet/inventory-display/inventory-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventory-display.component.html',
+  styleUrl: './inventory-display.component.scss'
+})
+export class InventoryDisplayComponent {
+
+}
